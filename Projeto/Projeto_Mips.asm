6b00306715e3529943ef8ab60b34fe2ab9d9a67f
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-05-26 21:00:08 -0300

    Update

--- Projeto/Projeto_Mips.asm
+++ Projeto/Projeto_Mips.asm
@@ -1,1 +1,20 @@
+# Arquitetura e Organização de Computadores
+# Semestre: 2025.1
+# Alunos:
+# Álvaro Ribeiro Bezerra da Silva
+# Daniel Dionísio de Oliveira
+# Gabriel Felipe Pontes da Silva Farias
 
+# Questão 01
+
+
+.data
+    var: .asciiz "Opa"
+
+.text
+.globl main
+
+main:
+    la $a0, var
+    li $v0, 4
+    syscall
