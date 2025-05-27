cdc982d12f4b8640b4df68f08f2756ed8699545e
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-05-26 21:38:07 -0300

    Update

--- Questoes/Questao1/ItemA.asm
+++ Questoes/Questao1/ItemA.asm
@@ -9,8 +9,24 @@
 
 
 .data
+    barraN: .asciiz "\n"
+
+
+.macro quebra-linha
+    la $a0, barraN  #carrega a variável barraN em $a0
+    li $v0, 4       #chama a impressão
+    syscall         #executa a linha anterior
+.end_macro
+
+.macro encerrar
+    li $v0, 10      #chama o serviço para encerrar
+    syscall         #encerra o programa
+.end_macro
 
 .text
 .globl main
+main:
+
+
 
 
