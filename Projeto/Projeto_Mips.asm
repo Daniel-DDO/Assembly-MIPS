3efef0ca8da379b7a1853fee92331301f8b07901
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-06-03 07:34:38 -0300

    Update

--- Projeto/Projeto_Mips.asm
+++ Projeto/Projeto_Mips.asm
@@ -9,14 +9,24 @@
 
 
 .data
-    espaco: .space 26080
+	apartamentos: .space 17120
+	
+	# Estrutura do código:
+
+	# Offset - Campo
+	#    0   - numApartamento (int)
+	#    4   - quantidadePessoas (int)
+	#    8   - nomeCompleto (char[]) (cada pessoa 64 caracteres)
+	#  328   - inteiroVeiculo (0. nenhum; 1. uma moto; 2. duas motos; 3. um carro) (int)
+	#  332   - modeloVeiculo (char[]) (cada modelo 32 caracteres)
+	#  396   - corVeiculo (char[]) (cara cor 16 caracteres)
 
 .text
 .globl main
 
 main:
 	#rascunho
-	la $a0, espaco
+	la $a0, apartamentos
 	lb $t1, 4($a0)
 	
 	li $t2, 1
